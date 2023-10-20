c04c835d6e1d54d8c7df8287e5bd59f196bfe139
Lucas Cohen <112208184+lucohen@users.noreply.github.com>
2023-10-19 19:37:37 -0700

    Add files via upload

---
+++ CUltimateMachine.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class CUltimateMachine {
+public:
+  CUltimateMachine() : m_bState(false) {
+  cout << "CUltimateMachine constructor (ctor) called" << endl;
+}
+
+~CUltimateMachine() {
+  cout << "CUltimateMachine destructor (dtor) called" << endl;
+}
+
+void displayState() {
+  switch (m_bState) {
+    case true:
+      cout << "Ultimate Machine is ON" << endl;
+    break;
+    case false:
+      cout << "Ultimate Machine is OFF" << endl;
+    break;
+  }
+}
+
+void turnOn() {
+  m_bState = true;
+  displayState();
+  if (true == m_bState)
+  {
+    m_bState = false;
+  }
+  displayState();
+}
+private:
+  bool m_bState;
+};
+
+int main() {
+  cout << "The Ultimate Machine Simulator" << endl;
+  CUltimateMachine ultmach;
+  ultmach.displayState();
+  ultmach.turnOn();
+}
